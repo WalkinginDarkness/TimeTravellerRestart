2a73e8b12ed35685809f6cc8c92c369989b993de
Art9 <xyc5000@hotmail.com>
2017-09-04 00:06:42 +0800

    新增八项移动
    脚本CharacterMove控制移动
    
    其属性MovingSpeed代表移动一格需要多少秒

---
+++ TimeTravelerRestart/Assets/Scripts/Controls/CharacterMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+	private float tileLength = 1.0f;
+	private float moveX = 0;
+	private float moveZ = 0;
+
+	/** while the player is moving, other actions are ignored.*/
+	private bool isMoving = false;
+	/** seconds need to move one tile.*/
+	public float movingSpeed;
+	private float movingSpeedInverse;
+	private float movingProgress = 0f;
+	private Vector3 movingStart;
+
+	// Use this for initialization
+	void Start () {
+		movingStart = transform.position;
+		movingSpeedInverse = 1.0f / movingSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isMoving) {
+			float mx = Input.GetAxisRaw ("Horizontal");
+			float mz = Input.GetAxisRaw ("Vertical");
+			if (mx != 0 || mz != 0) {
+				moveX = mx;
+				moveZ = mz;
+				isMoving = true;
+				movingProgress = Time.deltaTime * movingSpeedInverse;
+			}
+		} else {
+			movingProgress += Time.deltaTime * movingSpeedInverse;
+		}
+	}
+
+	void LateUpdate () {
+		if (isMoving) {
+			Vector3 movingEnd = movingStart + new Vector3 (moveX * tileLength, 0, moveZ * tileLength);
+			Vector3 p = Vector3.Lerp (movingStart, movingEnd, movingProgress);
+			transform.position = p;
+			if (movingProgress >= 1) {
+				moveX = 0;
+				moveZ = 0;
+				isMoving = false;
+				movingProgress = 0;
+				movingStart = movingEnd;
+			}
+		}
+	}
+}

74dd5f407da0280723144040412e33b8613dd254
Art9 <xyc5000@hotmail.com>
2017-09-03 23:23:16 +0800

    实现网格效果
    MapGenerator自动生成网格地图
    
    新增的Prefabs均为临时演示用素材

---
+++ TimeTravelerRestart/Assets/Scripts/Maps/MapGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+	public List<GameObject> elements;
+	public GameObject mapRoot;
+	public int radiusRange;
+
+	// Use this for initialization
+	void Start () {
+		int r = radiusRange;
+		for (int x = -r; x <= r; x++)
+			for (int z = -r; z <= r; z++) {
+				int k = Random.Range (0, elements.Count);
+				Instantiate (elements [k], new Vector3 (x, 0, z), Quaternion.identity, mapRoot.transform); 
+			}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

67bea6031f7c2e4ac777d419b1b2c924771a4501
Art9 <xyc5000@hotmail.com>
2017-09-04 18:25:04 +0800

    新增子弹飞行

--- TimeTravelerRestart/Assets/Scripts/Controls/BulletMove.cs
+++ TimeTravelerRestart/Assets/Scripts/Controls/BulletMove.cs
@@ -13,6 +13,6 @@ public class BulletMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (transform.forward * moveSpeed * Time.deltaTime);
+		transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
 	}
 }

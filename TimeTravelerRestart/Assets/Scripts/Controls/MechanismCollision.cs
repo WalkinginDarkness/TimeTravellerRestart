161aa0f70e1f0da76f13221208efe0abc9715feb
Art9 <xyc5000@hotmail.com>
2017-09-10 15:21:34 +0800

    增加了一个碰撞的例子效果

--- TimeTravelerRestart/Assets/Scripts/Controls/MechanismCollision.cs
+++ TimeTravelerRestart/Assets/Scripts/Controls/MechanismCollision.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class MechanismCollision : MonoBehaviour {
     public GameObject mechanism;
     public GameObject thisGameObject;
+	public ParticleSystem particles;
     void OnTriggerEnter(Collider collider) {
         GameObject cloneMechanism = Instantiate(mechanism, new Vector3(transform.position.x, collider.transform.position.y, transform.position.z), new Quaternion());
         cloneMechanism.GetComponent<ChildrenBulletParentNameSetterTool>().SetParentName(collider.GetComponent<SimpleMove>().playerID);
         Debug.Log("机关被触发");
-        Destroy(thisGameObject);
+		Destroy(thisGameObject);
+
+		// 播放粒子效果
+		particles.gameObject.SetActive (true);
+		Destroy (particles.gameObject, 3.0f);
     }
 }

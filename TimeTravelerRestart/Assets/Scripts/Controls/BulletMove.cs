913c6d51827933860f0186db3ed9a2764a0c1c1e
Art9 <xyc5000@hotmail.com>
2017-09-16 13:45:10 +0800

    原型测试
    修正移动：
    P1: WASD, JKL
    P2: 上下左右 123

--- TimeTravelerRestart/Assets/Scripts/Controls/BulletMove.cs
+++ TimeTravelerRestart/Assets/Scripts/Controls/BulletMove.cs
@@ -8,19 +8,25 @@ public class BulletMove : MonoBehaviour {
 	public float rotateSpeed;
 	public Vector3 direction;
     public DestroyCallbackAbstract destroyCallbackObject;
-    public bool isBulletSpeedAcce = true;
+
+	private ParticleSystem[] pss;
 
     //设置从属关系，以便查询角色状态从而决定实际移动速度
     private string parentName;
 
 	void Start () {
         Destroy(gameObject, 20);
+		if (pss == null)
+			pss = GetComponentsInChildren<ParticleSystem> ();
+		if (pss == null || pss.Length == 0)
+			Debug.LogWarning ("no ParticleSystem found in Rocket!");
 	}
 
 	void Update () {
         BulletMovement();
         BulletRotation();
-    }
+		ParticleSystemSpeed ();
+	}
 
     public void SetParentName(string parentName) {
         this.parentName = parentName;
@@ -38,8 +44,18 @@ public class BulletMove : MonoBehaviour {
         transform.RotateAround(transform.position, transform.forward, rotateSpeed * Time.deltaTime);
     }
 
+	private void ParticleSystemSpeed() {
+		// change simulation speed
+		foreach (var ps in pss) {
+			var main = ps.main;
+			float ratio = GetPlayerBulletSpeedProperty();
+			main.simulationSpeed = ratio * 1.0f;
+		}
+		// get ratio
+	}
+
     private float GetPlayerBulletSpeedProperty() {
-        return isBulletSpeedAcce ? (PlayerStatusController.playerBulletSpeed.ContainsKey(parentName) ? PlayerStatusController.playerBulletSpeed[parentName] : 1.0f) : 1.0f;
+        return PlayerStatusController.playerBulletSpeed.ContainsKey(parentName) ? PlayerStatusController.playerBulletSpeed[parentName] : 1.0f;
     }
 
     private void OnDestroy() {
--- TimeTravelerRestart/Assets/Scripts/Controls/SkillSpeedUp.cs
+++ TimeTravelerRestart/Assets/Scripts/Controls/SkillSpeedUp.cs
@@ -7,30 +7,48 @@ public class SkillSpeedUp : TimeLimitedSkillAbstract {
     public float shootSpeed = 1.0f;
     public float moveSpeed = 1.0f;
     public float bulletSpeed = 1.0f;
+	public float simulationSpeed = 1.0f;
 
     private float oldShootSpeed = 1.0f;
     private float oldMoveSpeed = 1.0f;
     private float oldBulletSpeed = 1.0f;
+	private float oldSimulationSpeed = 1.0f;
+
+	private ParticleSystem ps;
+
+	void Start() {
+		ps = GetComponentInChildren<ParticleSystem> ();
+		if (ps == null) {
+			Debug.LogError ("no ParticleSystem found!");
+		}
+	}
 
     public override void BeforeSkill() {
-        base.BeforeSkill();
+        //base.BeforeSkill();
         oldShootSpeed = (int)PlayerStatusController.playerShootSpeed[this.GetComponent<SimpleMove>().playerID];
         oldMoveSpeed = (int)PlayerStatusController.playerMoveSpeed[this.GetComponent<SimpleMove>().playerID];
         oldBulletSpeed = (int)PlayerStatusController.playerBulletSpeed[this.GetComponent<SimpleMove>().playerID];
         oldPowerConsumeSpeed = (int)PlayerStatusController.playerPowerConsumeSpeed[this.GetComponent<SimpleMove>().playerID];
-        //oldPlayer
+		oldSimulationSpeed = ps.main.simulationSpeed;
+		//oldPlayer
         PlayerStatusController.playerShootSpeed[this.GetComponent<SimpleMove>().playerID] = oldShootSpeed * shootSpeed;
         PlayerStatusController.playerMoveSpeed[this.GetComponent<SimpleMove>().playerID] = oldMoveSpeed * moveSpeed;
         PlayerStatusController.playerBulletSpeed[this.GetComponent<SimpleMove>().playerID] = oldBulletSpeed * bulletSpeed;
         PlayerStatusController.playerPowerConsumeSpeed[this.GetComponent<SimpleMove>().playerID] = powerConsumeSpeed;
+		var main = ps.main;
+		main.simulationSpeed = oldSimulationSpeed * simulationSpeed;
+		main.startLifetimeMultiplier = 3.0f;
     }
 
     public override void AfterSkill() {
-        base.AfterSkill();
+        //base.AfterSkill();
         PlayerStatusController.playerShootSpeed[this.GetComponent<SimpleMove>().playerID] = oldShootSpeed;
         PlayerStatusController.playerMoveSpeed[this.GetComponent<SimpleMove>().playerID] = oldMoveSpeed;
         PlayerStatusController.playerBulletSpeed[this.GetComponent<SimpleMove>().playerID] = oldBulletSpeed;
         PlayerStatusController.playerPowerConsumeSpeed[this.GetComponent<SimpleMove>().playerID] = oldPowerConsumeSpeed;
+		var main = ps.main;
+		main.simulationSpeed = oldSimulationSpeed;
+		main.startLifetimeMultiplier = 1.0f;
     }
 
     public override void AdditionalUpdate()

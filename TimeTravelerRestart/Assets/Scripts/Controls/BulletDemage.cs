205ce0b7b31f8014f5defdbb5fee83110c206d2d
CheckPi <a630042336@live.com>
2017-09-17 21:33:03 +0800

    新增子弹循环穿墙模式
    添加在DestroyByBoundary脚本，还挺带感的，主要是子弹粒子好看哈哈哈

--- TimeTravelerRestart/Assets/Scripts/Controls/BulletDemage.cs
+++ TimeTravelerRestart/Assets/Scripts/Controls/BulletDemage.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class BulletDemage : MonoBehaviour
 {
-
-    // 子弹碰撞进入检测后，attack，再调取player的takedemage
-    // 就是调用另一脚本的函数
-    // 写得不好，要两种子弹
     public int attackDamage;
 
     GameObject hitObject;
---
+++ TimeTravelerRestart/Assets/Scripts/Controls/DestroyByBoundary.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BulletDestroyStyle
+{
+    DirectDestroy,
+    LoopTeleport
+};
+public class DestroyByBoundary : MonoBehaviour {
+
+    BoxCollider boundary;
+
+    [SerializeField]
+    public BulletDestroyStyle bulletDestroyStyle;
+
+
+    void OnTriggerExit(Collider other)
+    {
+        //Debug.Log(other.gameObject.transform.position);
+        //Debug.Log(other.gameObject.transform.forward);
+
+        if (other.gameObject.tag == "Bullet")
+        {
+            boundary = this.GetComponent<BoxCollider>();
+            GameObject bullet = other.gameObject;
+
+            if (bulletDestroyStyle == BulletDestroyStyle.DirectDestroy)
+            {
+                Destroy(bullet);
+            }
+
+            if (bulletDestroyStyle == BulletDestroyStyle.LoopTeleport)
+            {
+                // transform.forward前进方向是1.0or-1.0，但其他也会有一点0.1之类的
+                // 所以就判断0.9了
+                if (bullet.transform.forward.x < 0.9)
+                {
+                    bullet.transform.position += new Vector3(boundary.size.x, 0.0f, 0.0f);
+                }
+
+                if (bullet.transform.forward.x > -0.9)
+                {
+                    bullet.transform.position -= new Vector3(boundary.size.x, 0.0f, 0.0f);
+                }
+
+                if (bullet.transform.forward.z < 0.9)
+                {
+                    bullet.transform.position += new Vector3(0.0f, 0.0f, boundary.size.z);
+                }
+
+                if (bullet.transform.forward.z > -0.9)
+                {
+                    bullet.transform.position -= new Vector3(0.0f, 0.0f, boundary.size.z);
+                }
+            }            
+        } 
+    }
+}

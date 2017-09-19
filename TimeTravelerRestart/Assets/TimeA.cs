7331a9fb5d00ab727ea13538f93c470363a6dbe6
Kayabaakiko <464186281>
2017-09-19 09:45:48 +0800

    做了两个技能的粒子效果

---
+++ TimeTravelerRestart/Assets/TimeA.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeA : MonoBehaviour {
+
+	//public BulletMove bm;
+	//private GameObject[] bullets;
+
+	// Use this for initialization
+	void Start () {
+		//bullets = GameObject.FindGameObjectsWithTag ("Bullet");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		
+	}
+
+//	void OnCollisionEnter(Collision collision){
+//		GameObject b = collision.gameObject;
+//		if (b.tag=="Bullet") {
+//			BulletMove script = b.GetComponents<BulletMove> ();
+//			script.moveSpeed++;
+//		}
+
+//	}
+}

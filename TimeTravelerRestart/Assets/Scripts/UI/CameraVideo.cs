fdd65bc941ed696b1dd5417362825cecb4fe07ce
zzgyy123 <zzgyy123@163.com>
2017-09-22 00:20:34 +0800

    开场动画提交

---
+++ TimeTravelerRestart/Assets/Scripts/UI/CameraVideo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class CameraVideo : MonoBehaviour {
+
+    public VideoPlayer videoPlayer;
+    public GameObject canvas;
+
+    public float videoAlphaSpeed;
+    public float targetAlphaValue = 0.5f;
+
+    private bool bIsEnd = false;
+	// Use this for initialization
+	void Start () {
+        canvas = GameObject.FindGameObjectWithTag("StartSceneUI");
+        canvas.SetActive(false);
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.loopPointReached += EndReached;
+        //videoPlayer.Play();
+        //videoPlayer.prepareCompleted+=Prepared;
+	}
+
+    private void Update(){
+        if (bIsEnd)
+        {
+            //Debug.Log(videoPlayer.targetCameraAlpha);
+            if (videoPlayer.targetCameraAlpha > targetAlphaValue)
+            {
+                videoPlayer.targetCameraAlpha = videoPlayer.targetCameraAlpha-videoAlphaSpeed*Time.deltaTime;
+            }
+            else
+            {
+                canvas.SetActive(true);
+                
+                videoPlayer.enabled = false;
+            }
+        }
+    }
+
+    void Prepared(VideoPlayer videoPlayer){
+        Debug.Log("Prepared");
+    }
+
+    void EndReached(VideoPlayer videoPlayer)
+    {
+        bIsEnd = true;
+    }
+}
